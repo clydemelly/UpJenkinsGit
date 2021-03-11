7048ecc35ef03cf5a8d60181f5db6f89aeefe316
melwy <melwy@LAPTOP-TKV85N8S.mshome.net>
2021-03-11 13:40:24 -0600

    change

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -1,12 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 	@RestController
 	public class PersonController {
 		
-		@RequestMapping("/greeting")
+		@GetMapping("/greeting")
 	    public String greeting() {
 	        return "Git Spring Boot and Jenkins";
 	    }
